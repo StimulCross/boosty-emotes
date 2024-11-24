bc7d9dd6b44dcff67fa06a32c1b74ed399245133
Stimul Cross <stimulcross@gmail.com>
2024-11-24 16:30:35 +0500

    feat(shared): create type for emote picker tabs

---
+++ src/shared/types/emote-picker-tab.ts
@@ -0,0 +1,3 @@
+import { type EmoteProvider } from '@shared/types/index';
+
+export type EmotePickerTab = EmoteProvider | 'favorite';
--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,3 +1,4 @@
 export * from './emotes.types';
 export * from './messages';
 export * from './theme';
+export * from './emote-picker-tab';
